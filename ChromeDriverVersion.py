0f7cc9dbc6b7f14e30db1a5c57ec08d7b7b2f1e5
Mathis Van Eetvelde <Mathisco-01@users.noreply.github.com>
2019-03-07 11:03:27 +0100

    auto unzip chromedrivers
    added a feature that auto unzips the chromedriver zip to reduce github filesize
    
    changed chromedriver location to make more sense

--- ChromeDriverVersion.py
+++ ChromeDriverVersion.py
@@ -1,30 +1,67 @@
 import sys
+import os
 import errorLog
 
-macpath = "Docs/macosdriver/chromedriver"
-windowspath = "Docs/windowsdriver/chromedriver.exe"
-linuxpath = "Docs/linuxdriver/chromedriver"
+chromedriverMainPath = "chromedrivers"
+
+macpath = chromedriverMainPath + "/macosdriver/"
+macchromedriverpath = macpath + "chromedriver"
+zipmacpath = macchromedriverpath + "_mac64.zip"
+
+windowspath = chromedriverMainPath + "/windowsdriver/"
+windowschromedriverpath = windowspath + "chromedriver.exe"
+zipwindowspath = windowschromedriverpath[:-4] + "_win32.zip" 
+
+linuxpath = chromedriverMainPath + "/linuxdriver/"
+linuxchromedriverpath = linuxpath + "chromedriver"
+ziplinuxpath = linuxchromedriverpath + "_linux64.zip"
+
 
 currentPath = ""
 
-os = sys.platform
+workingOS = sys.platform
 
+def unzip(zippath, ospath):
+	import zipfile
+	currentZip = zipfile.ZipFile(zippath, 'r')
+	currentZip.extractall(ospath)
+	currentZip.close()
+	if(workingOS == "linux" or workingOS =="darwin"):
+		os.chmod(ospath + "chromedriver", 0o755)
 
 def getPath():
 	try:
 		global currentPath
 		if(currentPath == ""):
-			if(os == "linux"):
-				currentPath = linuxpath
-			elif(os == "win32" or os == "cygwin"):
-				currentPath = windowspath
-			elif(os == "darwin"):
-				currentPath = macpath
+			if(workingOS == "linux"):
+				if(os.path.exists(linuxchromedriverpath)):
+					currentPath = linuxchromedriverpath
+					print("Linux ChromeDriver found: " + currentPath)
+				else:
+					unzip(ziplinuxpath, linuxpath)
+					getPath()
 
-			print("ChromeDriver found: " + currentPath)
+			elif(workingOS == "win32" or os == "cygwin"):
+				if(os.path.exists(macchromedriverpath)):
+					currentPath = windowschromedriverpath
+					print("Windows ChromeDriver found: " + currentPath)
+				else:
+					unzip(zipwindowspath, windowspath)
+					getPath()
+
+			elif(workingOS == "darwin"):
+				if(os.path.exists(macchromedriverpath)):
+					currentPath = macchromedriverpath
+					print("macOS ChromeDriver found: " + currentPath)
+				else:
+					unzip(zipmacpath, macpath)
+					getPath()
+
+			
 			return currentPath
 		else:
 			return currentPath
 	except Exception as e:
 		print(e)
 		errorLog.log(e)
+
